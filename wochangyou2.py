b3c9e3e29d798d0c9c999cb2565d392a3416a658
xiaoge <2381919454@qq.com>
2023-05-16 17:26:47 +0800

    沃畅游

--- wochangyou2.py
+++ wochangyou2.py
@@ -6,7 +6,8 @@ import json
 
 url = 'https://game.wostore.cn/api/app/user/v2/qos/start'
 headers = {
-    'authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjo4Mjc2MzY0NjIyOTY0NjU0MDgsInVzZXJfa2V5IjoiMTkzMzJlYjMtZTRhNy00ZmVjLTllN2EtZjdhOTg2MDE3NThlIiwidXNlcm5hbWUiOiJsKuaYjiJ9.AG5aZAC7q8QRDE2FJZrZylaGSS1fJeb9Phgz4bl2LUHCNB2wfW-jZpsAmmAJ3DLZDFbsWghWV8SPxV9Kf8z_tQ',
+    'authorization': 'eyJhbGciOiJIUzUxMiJ9.eyJ1c2VyX2lkIjo4Mjc2MzY0NjIyOTY0NjU0MDgsInVzZXJfa2V5IjoiNWQxOTdkN2YtNDEyOC00M2RlLWEwZDktNGE3ZWRjZTE5MmQ5IiwidXNlcm5hbWUiOiJsKuaYjiJ9.0aGm6G9sm5x2KuGC2T9CCHaM1H8ClS5qIRGvRXoPDM1s7L7vR754D-Vv5jHnoEttM9h-ezghKf8Z8cHCQdX3CA',
+    'user-agent': 'okhttp/4.9.2',
     'content-type': 'application/json'
 }
 body = {"channelId": "90002", "privateIp": "10.1.10.1"}
