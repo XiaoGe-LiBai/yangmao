4789c4076c3d8403010ce8e2202e045f7197ba54
XiaoGe-LiBai <60430782+XiaoGe-LiBai@users.noreply.github.com>
2024-06-01 22:56:38 +0800

    Update qdw.py

--- qdw.py
+++ qdw.py
@@ -16,7 +16,7 @@ accounts = os.getenv('qdw_sid')
 if accounts is None:
     print('你没有填入qdw_sid，咋运行？')
 else:
-    accounts_list = os.environ.get('qdw_sid').split('@')
+    accounts_list = os.environ.get('qdw_sid').split('&')
     num_of_accounts = len(accounts_list)
     print(f"获取到 {num_of_accounts} 个账号")
     for i, account in enumerate(accounts_list, start=1):
@@ -48,36 +48,3 @@ else:
         response = requests.get(url, headers=headers).json()
         if response['code']==0:
             print(f"账号【{str(sid)[:18]}】 连续签到【{re['data']['continuesDay']}天】\n当前可用积分:{response['data']['member']['stats']['points']}")
-
-if __name__ == '__main__':
-    # 加载通知
-    load_send()
-
-    # 抢购
-    maotai_configs = get_envs(env_key)
-    msg = ''
-    index = 1
-    for config in maotai_configs.split("&"):
-        single_msg = f'===== 第{index}个账号 =====\n'
-        if not config:
-            continue
-        province, city, lng, lat, device_id, token, ck = config.split(',')
-        time_keys = str(int(time.mktime(datetime.date.today().timetuple())) * 1000)
-        try:
-            get_map()
-            session_id, item_codes = get_session_id(device_id, token)
-            user_name, user_id, mobile = get_user_id(token, device_id)
-            for item_code in item_codes:
-                name = res_map.get(str(item_code))
-                shop_id = get_shop_item(session_id, item_code, device_id, token, province, city)
-                res = mt_add(item_code, str(shop_id), session_id, user_id, token, device_id)
-                single_msg += f'{user_name}({mobile}) {name} 签到结果: {res}\n'
-            r = getUserEnergyAward(device_id, ck)
-            single_msg += f'{user_name}({mobile} 耐力: {r}\n'
-        except Exception as e:
-            single_msg += f'异常: {e}'
-        logging.info(single_msg)
-        msg += single_msg + '\n\n'
-        index += 1
-
-        send('七点五饮用天然矿泉水_小程序_签到结果', msg)
