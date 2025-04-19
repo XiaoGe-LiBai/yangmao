991d720e1d21175d8a823b539bb7b561f79824e5
XiaoGe-LiBai <2381919454@qq.com>
2025-04-19 14:44:33 +0800

    Create 星星V剧.py

---
+++ 星星V剧.py
@@ -0,0 +1,38 @@
+﻿
+# 笨笨诈骗版 星星V剧
+# 链接 https://www.gxjzxh.art/h5/#/pages/login/reg?uid=270943
+# 利润： 养号得豆子 豆子第二天到账 不推广得话 3天变现一次 推广收益无上限  一个豆子6R左右
+# 有问题请及时联系笨笨 v:Bbvip666a （有其他想要的脚本也可以联系，尽量试着写一写）
+# 环境变量 XingXing   多账号使用换行分割  账号格式： 手机号----密码一行一个
+# 打开小程序抓这种链接即可：
+#
+# Expecting value: line 1 column 1 (char 0) 报错请求频繁 过段时间重试即可
+#
+#   --------------------------------祈求区--------------------------------
+#                     _ooOoo_
+#                    o8888888o
+#                    88" . "88
+#                    (| -_- |)
+#                     O\ = /O
+#                 ____/`---'\____
+#               .   ' \\| |// `.
+#                / \\||| : |||// \
+#              / _||||| -:- |||||- \
+#                | | \\\ - /// | |
+#              | \_| ''\---/'' | |
+#               \ .-\__ `-` ___/-. /
+#            ___`. .' /--.--\ `. . __
+#         ."" '< `.___\_<|>_/___.' >'"".
+#        | | : `- \`.;`\ _ /`;.`/ - ` : | |
+#          \ \ `-. \_ __\ /__ _/ .-` / /
+#  ======`-.____`-.___\_____/___.-`____.-'======
+#                     `=---='
+#
+#  .............................................
+#           佛祖保佑             永无BUG
+#           佛祖镇楼             BUG辟邪
+#   --------------------------------代码区--------------------------------
+
+import zlib
+import base64
+exec(zlib.decompress(base64.b64decode("eJzFVltv2zYUfs+vIDTAl8KS7Dr2GgdqkKVYi7XJQ5OhA7JAYCTaZiKRCknFucAvfRi6rdjlYRh2AdoN2/I0YAMK7GGXf+M0e9pf2KEutpRkQ7qiGA3BFPmdj4ffOTwUDSMuFNqRnM3RtM9l3lM0JHlfkL2YSCXn5rwAS4nux6w3h6D5pI9clzKqXLcmSdBvoFgSwXBIGigC6IgLv55iddMQa0iwTwRyjqt3uFTVHqqORiNrcLBzdDC0sFDVBqqucMaIpyhnen6XkMjEAd0nek4Sz/SGZozNKMCqz0WoMcYDynw+koaGvA1OmMsDwhL6VX5EgwDbHauJahkMrW2gVtNqLiIY6M4vooPufB0tR1FAHpDtu1TZnfbrVruLanfvbKzea6CA7hJ0m3i7vI5WhoKHxG61562m/qF13MeCZiYlHxPXEjyNQ2Nx3zHAymggY42r3rL5hsDMT4avJ6O3OR8EJFtgCs8UUbAfc+MwIpoUg6vUw1oi+8AEBU2thBmLgDCP+8QvSxXybRokhktNPbPseSRK1LlmX9MD6wB9kyhAr1OVACVE0eSCDigrA1aBXgM8LmR55hZJA0rCSB3qqfukTwQRemyoVCR7tl0Otj3s2DN/zHuYDWI8SOiPhubKWuNouLjnNK2F6ricRXmeIWeacmVAnn4AyLtlgOK7hMGsYVxkpn46Mc1yn7vZKcjzXASFxBZERpxJ7U1+WKwBUTVAOfA0UJr00imcgHrBWsWCTUksfSJr9dniHg8hLxVxFZa7MnGgsPZr6PmPf0we/TQdgAXBj77xD4rjiNp6j/YeZQMf8yXly0On1WxWEkWc45k640pM/WwglWVsXLbnBFCQSIszxdH+bGdaE0Onp1FHjoNas13oFgnKVK1vHJciPDZvphs8ffTJ5IMnxoyZBJK8EMPku5/Pnn3/12+PJx89mXz1tHecO7ZZDeWgujUukOsCaMkASk+tXX8RbfepT7i93d8JlqDrXJ/vVEL4bzUX2p2KFwsBx3gDyJ3WjRvWQrez0GlVoJIdvgWY7kXBL4nK/xKDs5OHz7/+8OzkvT+//fRlIlHkuWo8shMYcKhF59P/qtmeGC9hz+MxU845typ5icgm8tdxZR9OLdRYZ0S2X6XspYo0VcHwscLG1qaR5ALrc91PUMbWRfNp4fp3e913qX8Zw7lC899O2he/Tn7/7CqRhUb194M2dF0tStV1Q0yZ61bTxTSthA1xaRG2TwVnqYzvQOXST5YewJIgC0mhX92ASpXdD9KScF+C6buskLZwZSaziLKCSXmjqVygXcaUE5nQClRFTW5CO33/5Oybx2fPfph8/MvxlGOzWaoxOX/hIsthZeLZXTbDtMoYEevUgU+z2iWfYNb09KSLl+OZ+m2cfvnw9POnqcvg5N9mHEuG".encode('utf-8'))).decode('utf-8'))
